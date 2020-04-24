29b58fa3c9abf9e0a66cb96baa9ebced878f0444
Gabriel Passos <contato.gabrielhp@gmail.com>
2020-04-24 20:08:06 -0300

    Desafio finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('Você não possui saldo suficiente.');
+    }
+
+    if (type === 'income' || type === 'outcome') {
+      const transactionsRepository = this.transactionsRepository.create({
+        title,
+        value,
+        type,
+      });
+      return transactionsRepository;
+    }
+
+    throw new Error('Tipo inválido.');
   }
 }
 
